cb22ba3a07c9048890c0f20af199629dd073089f
MikeMaesPXL <11901518@student.pxl.be>
2023-12-17 18:08:04 +0100

    Added some 'suprise' on the confirmation page -> have to finish confirmation and checkout tommorow.

--- src/views/confirmationView.vue
+++ src/views/confirmationView.vue
@@ -1,32 +1,64 @@
 <script>
+import JSConfetti from 'js-confetti'
+
+export default {
+    data() {
+        return {
+            jsConfetti: new JSConfetti(),
+        }
+    },
+    computed: {
+        addConfetti() {
+            this.jsConfetti.addConfetti();
+        },
+        home() {
+          this.$router.push('/');
+        },
+    }
+}
 
 </script>
 <template>
     <body>
-       <h1>THANK YOU FOR YOUR PURCHASE!</h1>
-       <button class="backToHome__button" @click="checkout">Confirm</button>
+        <div class="wrapper">
+            <h1>THANK YOU FOR YOUR PURCHASE!</h1>
+            <p>{{ addConfetti }}</p>
+            <button class="backToHome__button" @click="home">Confirm</button>
+        </div>
     </body>
 </template>
 <style lang="scss" scoped>
     @import '../scss/base.scss';
 
-    h1 {
-        margin-top: 250px;
-    }
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-    .backToHome__button {
-        margin-top: 20px;
-        align-self: flex-end;
-        font-size: 16px;
-        background-color: $color-primary;
-        color: $color-light;
-        border: none;
-        padding: 10px 20px;
-        border-radius: 7px;
-        cursor: pointer;
+        .wrapper {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
 
-        &:hover {
-            background-color: darken($color-primary, 10%);
+            h1 {
+                margin-top: 250px;
+            }
+
+            .backToHome__button {
+                margin-top: 20px;
+                align-self: flex-end;
+                font-size: 16px;
+                background-color: $color-primary;
+                color: $color-light;
+                border: none;
+                padding: 10px 20px;
+                border-radius: 7px;
+                cursor: pointer;
+
+                &:hover {
+                    background-color: darken($color-primary, 10%);
+                }
+            }
         }
     }
 </style>
